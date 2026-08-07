3bfe20d8b4b10aef63c52dbfd6cf9b83d861317e
unknownshopper <the@unknownshoppers.com>
2026-08-07 12:26:13 -0600

    ¨ignore¨

---
+++ config.local.js
@@ -0,0 +1,8 @@
+window.PCT_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCmutjS09d1hCjwlhuH0upkGr_TIVVBktc",
+  authDomain: "xpct-tab.firebaseapp.com",
+  projectId: "xpct-tab",
+  storageBucket: "xpct-tab.firebasestorage.app",
+  messagingSenderId: "614589989974",
+  appId: "1:614589989974:web:e89a808180e4b563a8e7f7"
+};  
